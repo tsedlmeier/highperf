3bb2a510178d45cd90e1b5ef7331a62543d4627c
tsedlmeier <toni-sedlmeier@gmx.de>
2022-11-02 11:30:36 +0100

    pointer solution

--- muenze/src/muenze2.c
+++ muenze/src/muenze2.c
@@ -2,24 +2,42 @@
 #include "stdlib.h"
 
 #define N 16
+#define LEN 6
 // N - Anzahl an 1er = Anzahl der Stellen
 // z.B   1 1 4 --> 001110 --> 6-3 = 3 Stellen (1 1 4)
+int A[LEN] = {1,1,1,1,1,1};
+
+void print(int *a)
+{
+    for (size_t i = 0; i < sizeof(A)/sizeof(int) ; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
+void shift(int *a)
+{
+    for (size_t i = LEN; i > 0 ; i--)
+    {
+        if (a[i]==0) {
+            break;
+        }
+        a[i-1] = a[i];
+    }
+}
 
 int main(void)
 {
-    int k=1,y=1,z=1;
-    int n = 3;
+    int *y = &A[LEN-2];
+    int *z = &A[LEN-1];
     for (size_t i = 0; i < N ; i++)
     {
-        for (size_t j = 0; j < n ; j++)
+        print(A);
+        if( *z == 1 ) 
         {
-            printf("%d ", k);
+            (*z)++; 
+            shift(A);
         }
-        printf("%d %d", y,z);
-        if( (z--) == 0 ) 
-        {
-            n--;
-            y++;
+        else {
         }
     printf("\n");
     }
